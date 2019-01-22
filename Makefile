a82e32564d39e5a441b91cd4aea9cef9f8aec469
Gunnar Þór Magnússon <gunnar.thor.magnusson@gmail.com>
2019-01-22 05:50:29 +0100

    Print help and version messages

--- Makefile
+++ Makefile
@@ -1,9 +1,23 @@
-src=main.go
-bin=owl
+src := main.go
+bin := owl
+
+date := $(shell date '+%Y-%m-%d_%H:%M:%S')
+epoch := $(shell date '+%s')
+hash := $(shell git rev-parse --short HEAD)
+LDFLAGS :=
 
 $(bin): $(src)
-	go build -o $(bin) $(src)
+	go build $(LDFLAGS) -o $(bin) $(src)
+
+release: LDFLAGS = -ldflags "-X main.BuildDate=$(date) -X main.BuildEpoch=$(epoch) -X main.BuildHash=$(hash)"
+
+.PHONY: release
+release: $(bin) repo
 
 .PHONY: clean
 clean:
 	rm -f $(bin)
+
+.PHONY: repo
+repo:
+	git diff --quiet HEAD
--- main.go
+++ main.go
@@ -1,9 +1,82 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"strings"
+)
+
+var (
+	BuildDate  = ""
+	BuildEpoch = ""
+	BuildHash  = ""
 )
 
 func main() {
-	fmt.Println("Hello")
+	flags := struct {
+		helpShort *bool
+		helpLong  *bool
+		version   *bool
+	}{
+		helpShort: flag.Bool("h", false, "Print help and exit"),
+		helpLong:  flag.Bool("help", false, "Print help and exit"),
+		version:   flag.Bool("version", false, "Print version and exit"),
+	}
+	flag.Parse()
+
+	if *flags.helpShort || *flags.helpLong {
+		fmt.Println(msgHelp())
+		return
+	}
+
+	if *flags.version {
+		fmt.Println(msgVersion())
+		return
+	}
+}
+
+func msgHelp() string {
+	return strings.TrimSpace(`
+owl - A simple work tracker
+
+USE:
+
+	owl [OPTION...] COMMAND
+
+COMMANDS:
+
+	start		Start a session
+	stop		Stop a session
+	status		Print owl status
+	report		Print a report of work done
+
+OPTIONS:
+
+	-h, --help	Print help and exit
+	--version	Print version and exit
+`)
+}
+
+func msgVersion() string {
+	if BuildDate == "" && BuildEpoch == "" && BuildHash == "" {
+		return "Development version"
+	}
+
+	if BuildDate == "" {
+		BuildDate = "missing"
+	}
+
+	if BuildEpoch == "" {
+		BuildEpoch = "missing"
+	}
+
+	if BuildHash == "" {
+		BuildHash = "missing"
+	}
+
+	return strings.TrimSpace(fmt.Sprintf(`
+Build date: %s
+Build epoch: %s
+Build commit hash: %s
+`, BuildDate, BuildEpoch, BuildHash))
 }
