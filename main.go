74bebe8914f723ad00ce2b5cad681d50a48bce3f
Gunnar Þór Magnússon <gunnar.thor.magnusson@gmail.com>
2019-01-22 06:28:44 +0100

    Setup config and data folders

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+	"path"
 	"strings"
 )
 
@@ -39,6 +41,11 @@ func main() {
 		return
 	}
 
+	if err := initFolders(); err != nil {
+		fmt.Fprintf(os.Stderr, "Couldn't initialize directories: %s\n", err)
+		os.Exit(1)
+	}
+
 	switch flag.Args()[0] {
 	case "start":
 		cmdStart()
@@ -62,6 +69,40 @@ func cmdStop()   {}
 func cmdStatus() {}
 func cmdReport() {}
 
+func initFolders() error {
+	if err := os.MkdirAll(dirConfig(), 0755); err != nil {
+		return err
+	}
+
+	if err := os.MkdirAll(dirData(), 0755); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func dirConfig() string {
+	if dir, ok := os.LookupEnv("XDG_CONFIG_HOME"); ok {
+		return dir
+	}
+
+	return path.Join(os.Getenv("HOME"), ".config")
+}
+
+func fileConfig() string {
+	return path.Join(dirConfig(), "owl.cfg")
+}
+
+func dirData() string {
+	if dir, ok := os.LookupEnv("XDG_DATA_HOME"); ok {
+		return path.Join(dir, "owl")
+	}
+
+	return path.Join(os.Getenv("HOME"), ".local", "share", "owl")
+}
+
+func fileState() string {
+	return path.Join(dirData(), "owl.state")
 }
 
 func msgHelp() string {
